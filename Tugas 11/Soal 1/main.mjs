0100a23a5e8e4bfab01c6c894e4a50e5551827d5
HANS <151702640+HanzxRYU@users.noreply.github.com>
2024-09-04 13:25:46 +0700

    update

---
+++ Tugas 11/Soal 1/main.mjs
@@ -0,0 +1,13 @@
+// main.js
+
+// Impor pi, add, dan subtract dari math.js
+import { pi, add, subtract } from "./math.mjs";
+
+// Tampilkan nilai pi ke konsol
+console.log(`Nilai pi: ${pi}`);
+
+// Tampilkan hasil dari add(5, 3) ke konsol
+console.log(`Hasil add(5, 3): ${add(5, 3)}`);
+
+// Tampilkan hasil dari subtract(10, 4) ke konsol
+console.log(`Hasil subtract(10, 4): ${subtract(10, 4)}`);
