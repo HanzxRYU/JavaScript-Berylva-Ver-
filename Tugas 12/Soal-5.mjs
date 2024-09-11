c9bd29b6f0ec41396cf03be99dd1fc029edd74a1
HANS <151702640+HanzxRYU@users.noreply.github.com>
2024-09-11 10:50:21 +0700

    update Tugas 12

--- Tugas 12/Soal-5.mjs
+++ Tugas 12/Soal-5.mjs
@@ -1,3 +1,8 @@
-function delayedMessages(message, delay) {
-    
+function delayedMessage(message, delay) {
+  setTimeout(() => {
+    console.log(message); // Mencetak pesan setelah delay
+  }, delay);
 }
+
+// Memanggil fungsi dengan pesan "Hello after delay" dan delay 2000 milidetik
+delayedMessage("Hello after delay", 2000);
