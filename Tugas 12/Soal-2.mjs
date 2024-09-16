28733d71d6fd4150e763d47bdaa8d6405854563d
HANS <151702640+HanzxRYU@users.noreply.github.com>
2024-09-16 09:40:17 +0700

    update

--- Tugas 12/Soal-2.mjs
+++ Tugas 12/Soal-2.mjs
@@ -1,13 +1,13 @@
-// function printEverySecond() {
-//     let i = 0; // Variabel yang di-update tiap detik
-//     const time = setInterval(() => {
-//       console.log("Hello world"); // Cetak nilai i
-//       i++; // Naikkan nilai i
-//       if (i >= 5000) { // Contoh kondisi untuk menghentikan interval
-//         clearInterval(time); // Hentikan interval
-//       }
-//     }, 1000); // Interval waktu 1000 milidetik (1 detik)
-//   }
+function printEverySecond() {
+    let i = 0; // Variabel yang di-update tiap detik
+    const time = setInterval(() => {
+      console.log("Hello world"); // Cetak nilai i
+      i++; // Naikkan nilai i
+      if (i >= 5) { // Contoh kondisi untuk menghentikan interval
+        clearInterval(time); // Hentikan interval
+      }
+    }, 1000); // Interval waktu 1000 milidetik (1 detik)
+  }
   
-//   printEverySecond();
+  printEverySecond();
   
