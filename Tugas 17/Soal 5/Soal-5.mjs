83b67b6e930341e7b3fbf4eba7cd024e059e16a9
HANS <151702640+HanzxRYU@users.noreply.github.com>
2024-10-21 10:22:14 +0700

    Done

---
+++ Tugas 17/Soal 5/Soal-5.mjs
@@ -0,0 +1,5 @@
+const img = document.createElement("img");
+img.src = "https://picsum.photos/200/300";
+document.getElementById("image Container").appendChild(img);
+
+img.style.borderRadius = "50%";
